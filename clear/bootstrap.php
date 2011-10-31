26c32d2ea868ed6dc9ab3abaa19d02bae4b9c783
Didier Jean Charles <didijc@gmail.com>
2011-10-31 06:35:57 -0400

    the first round of code for the CLEAR Framework -- building the foundation

---
+++ clear/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+	//Yes these requires could be included directly in index.php -- but I am doing this to allow future expansion of the framework
+	require_once(ROOT.DS.'config'.DS.'config.php');
+	require_once(ROOT.DS.'clear'.DS.'shared.php');
